d1508b2eda58b56729931f63c86e14eabf1699e1
Mochamad Ludfi Rahman <ludfyrahman@192.168.1.7>
2023-06-27 12:30:30 +0700

    updated

--- resources/views/superadmin/dashboard/layout/sidebar.blade.php
+++ resources/views/superadmin/dashboard/layout/sidebar.blade.php
@@ -10,7 +10,7 @@
     <!-- Brand Logo -->
     <a href="/" class="brand-link">
       <img src="{{ asset('adminlte') }}/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">SISI</span>
+      <span class="brand-text font-weight-light">BACKEND</span>
     </a>
 
     <!-- Sidebar -->
